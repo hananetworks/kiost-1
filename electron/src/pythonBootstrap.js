d6926acbf0c127843fa53a650e9f4dfa31a46192
geonwoo92 <cnu201102054@gmail.com>
2025-11-20 10:48:19 +0900

    Fix: Correct DLL search path for python-env

--- electron/src/pythonBootstrap.js
+++ electron/src/pythonBootstrap.js
@@ -7,7 +7,7 @@ const { log } = require('./logging');
 const dotenv = require('dotenv'); // dotenv 모듈 필요
 
 // [설정]
-const REQUIRED_ENV_VERSION = 'env-v1.0.2';
+const REQUIRED_ENV_VERSION = 'env-v1.0.0';
 const REPO_OWNER = 'hananetworks';
 const REPO_NAME = 'kiost-1';
 
